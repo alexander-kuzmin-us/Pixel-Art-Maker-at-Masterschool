1166a44b50c5e4fdde8920630dd840533f58b45c
Alex Kuzmin <alex.kuzminn@gmail.com>
2022-01-17 21:09:13 -0500

    First commit
    Created event listener to get user input of Height and Width and store its value in the variables.

--- designs.js
+++ designs.js
@@ -1,7 +1,16 @@
 // Select color input
 // Select size input
+let userInputHigh, userInputWidth; //declare variables
+
+document.addEventListener("submit", function (a) { //add event listener to the document object
+    event.preventDefault(); //prevent default behavior of submit button
+    userInputHigh = document.getElementById("inputHeight").value; //locate element by id and assign its value to a var
+    userInputWidth = document.getElementById("inputWidth").value; //locate element by id and assign its value to a var
+    console.log(userInputHigh, userInputWidth); //test line will be comment out later
+});
 
 // When size is submitted by the user, call makeGrid()
+
 
 function makeGrid() {
 
